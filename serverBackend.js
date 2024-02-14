b51368fe197b95d73de35af6b2e95c62688816b7
taitt-e <123587963+taitt-e@users.noreply.github.com>
2024-02-14 11:16:19 -0600

    Create serverBackend.js
    The javascript file that will serve as a go in between the other javascript files and connecting with the main database server.

---
+++ serverBackend.js
@@ -0,0 +1,3 @@
+console.log("Hello World!")
+//Be sure to make Classes for functions as this provides modularity for future development.
+//Use this for all Microsoft Azure and SQL related functions to hide what is going on in the front end
