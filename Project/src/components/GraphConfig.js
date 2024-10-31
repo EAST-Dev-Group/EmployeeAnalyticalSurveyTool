b3f9c171e339733b582c4da19517ab1265c8fc64
taitt-e <123587963+taitt-e@users.noreply.github.com>
2024-10-31 12:06:56 -0500

    Update graphconfig prototype functions

--- Project/src/components/GraphConfig.js
+++ Project/src/components/GraphConfig.js
@@ -1,10 +1,17 @@
 //Imports Here
 
 //Functions Here
-function buildAxis(){
-    
+function buildAxis(graphType){
+    let scaleStr = "";
+    let dataArr = [];
+
 }
 
-function buildSeries(){
-    
+function buildSeries(graphType){
+    let idCount = 0;
+    let valueArr = [];
+    let labelArr = [];
+    let dataArr = [];
+
+
 }
