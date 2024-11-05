93ca2029790e10a8386361418ded50f4db4066dd
taitt-e <123587963+taitt-e@users.noreply.github.com>
2024-11-05 12:33:27 -0600

    Revert as the function is not working properly.

--- Project/src/components/BarGraph.js
+++ Project/src/components/BarGraph.js
@@ -1,21 +1,27 @@
 // src/components/BarGraph.js
 import React from 'react';
 import { BarChart } from '@mui/x-charts/BarChart';
-import { FilterDataGrid } from '../utils/GraphConfig';
+import { filterDataGrid } from '../utils/GraphConfig';
 
-const BarGraph = ({data}) => {
-  const inputData = FilterDataGrid(data);
+const BarGraph = () => {
+  const data = [
+    { rating: '1-Star', value: 10 },
+    { rating: '2-Star', value: 15 },
+    { rating: '3-Star', value: 25 },
+    { rating: '4-Star', value: 15 },
+    { rating: '5-Star', value: 20 },
+  ];
 
   return (
     <div style={{ width: '100%', height: 300, marginTop: '20px' }}>
       <BarChart
         xAxis={[{ 
-          data: inputData.map(item => item.rating),
+          data: data.map(item => item.rating),
           scaleType: 'band',
         }]}
         series={[
           {
-            data: inputData.map(item => item.value),
+            data: data.map(item => item.value),
             area: true,
           },
         ]}
