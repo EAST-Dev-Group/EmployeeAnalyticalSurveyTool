f2d981175f9c1331b008be321f8a6f26b78182cc
taitt-e <123587963+taitt-e@users.noreply.github.com>
2024-11-05 16:23:15 -0600

    To reduce redundancy and make this easier, made the current graph config specifically for bargraph configs. Will make separate configs for other graphs.

--- Project/src/components/BarGraph.js
+++ Project/src/components/BarGraph.js
@@ -1,19 +1,10 @@
 // src/components/BarGraph.js
 import React from 'react';
 import { BarChart } from '@mui/x-charts/BarChart';
-import { FilterDataGrid } from '../utils/GraphConfig';
+import { DefaultSatisfactionGraph } from '../utils/BarGraphConfig';
 
 const BarGraph = () => {
-  const data = FilterDataGrid();
-  //console.log(data);
-  /*const data = [
-    { rating: '1-Star', value: 10 },
-    { rating: '2-Star', value: 15 },
-    { rating: '3-Star', value: 25 },
-    { rating: '4-Star', value: 15 },
-    { rating: '5-Star', value: 20 },
-  ];
-  */
+  const data = DefaultSatisfactionGraph();
 
   return (
     <div style={{ width: '100%', height: 300, marginTop: '20px' }}>
---
+++ Project/src/utils/BarGraphConfig.js
@@ -0,0 +1,49 @@
+//Used to build Graphs Axis & Series, then return those arr vals.
+//Imports Here
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+//Functions & Vars Here
+export function DefaultSatisfactionGraph(){
+    const [chartData, setChartData] = useState([]);
+
+    //Fetches All Data from Server
+    const fetchAllData = async () => {
+        try {
+          const response = await axios.get('/api/allData');
+          processData(response.data);
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      }
+    
+    // Process data for whole Bar Chart
+    const processData = (data) => {
+      if (data && data.length > 0) {
+        // Group by Satisfaction Rating
+        const satisfactionCounts = data.reduce((acc, row) => {
+          const rating = row['Satisfaction Rating'];
+          acc[rating] = (acc[rating] || 0) + 1;
+  
+          return acc;
+        }, {});
+  
+        // Convert to array format for chart
+        const processedData = Object.entries(satisfactionCounts)
+          .map(([rating, count]) => ({
+            rating: Number(rating),
+            count: count
+          }))
+          .sort((a, b) => a.rating - b.rating);  // Sort by rating
+
+          setChartData(processedData);
+      }
+    };
+
+    //Used to break infinite loops.
+    if(chartData && chartData.length <= 0){
+        fetchAllData();
+    }
+    //outputData(chartData);
+    return chartData;
+}
