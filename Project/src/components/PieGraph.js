eab929d28ae00aa6f74927bb21049a563da372a5
taitt-e <123587963+taitt-e@users.noreply.github.com>
2024-11-09 16:47:41 -0600

    Added Label to the piechart

--- Project/src/components/PieGraph.js
+++ Project/src/components/PieGraph.js
@@ -2,25 +2,26 @@
 import * as React from 'react';
 import { PieChart } from '@mui/x-charts/PieChart';
 import { DefaultPieGraph } from '../utils/PieGraphConfig';
+import { Typography } from '@mui/material';
+import Box from '@mui/material/Box';
 
 const PieGraph = () => {
   const data = DefaultPieGraph();
 
   return (
     <div style={{ width: '100%', height: 300, marginTop: '20px' }}>
-      <PieChart
-      series={[
-        {
-          data: [
-            { id: 0, value: 10, label: 'series A' },
-            { id: 1, value: 15, label: 'series B' },
-            { id: 2, value: 20, label: 'series C' },
-          ],
-        },
-      ]}
-      width={400}
-      height={200}
-    />
+      <Box flexGrow={1}>
+        <Typography># of Ratings from Each CSIT Org:</Typography>
+        <PieChart
+        series={[
+          {
+            data: data,
+          },
+        ]}
+        width={800}
+        height={400}
+      />
+    </Box>
     </div>
   );
 };
