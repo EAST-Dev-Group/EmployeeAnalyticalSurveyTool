5b7d9f86c4fdf89c5baf5df669e4d2ceb98a7357
taitt-e <123587963+taitt-e@users.noreply.github.com>
2024-11-09 21:22:28 -0600

    sorting by dates works properly.

--- Project/src/utils/LineGraphConfig.js
+++ Project/src/utils/LineGraphConfig.js
@@ -27,10 +27,10 @@ export function DefaultCSITGraph(){
         let dataArr = [];
 
         for(let i = 0; i < rawDataArr.length; ++i){
-          dataArr.push({label: rawDataArr[i]["CSIT Org"], value: rawDataArr[i]["Satisfaction Rating"], date: rawDataArr[i]["Recorded Date"]});
+          dataArr.push({csit: rawDataArr[i]["CSIT Org"], rating: rawDataArr[i]["Satisfaction Rating"], date: rawDataArr[i]["Recorded Date"]});
         }
 
-        dataArr.sort((a, b) => Date(a.date) - Date(b.date)); // Sort by date
+        dataArr.sort((a, b) => new Date(a.date) - new Date(b.date)); // Sort by date
         const processedData = Object.assign(dataArr);
 
         setChartData(processedData);
